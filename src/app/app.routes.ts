4c3d642d36d74f58aa551056f4748e60a689287a
Philip Soares <philipsoares1@gmail.com>
2018-08-31 19:51:03 -0300

    Inclui separação dos módulos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,10 @@
 import { Routes } from '@angular/router'
 
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
-import { OrderComponent } from './order/order.component';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
 
 export const ROUTES: Routes = [
@@ -19,8 +17,8 @@ export const ROUTES: Routes = [
             {path: 'reviews', component: ReviewsComponent}
         ]
     },
-    {path: 'about', component: AboutComponent},
-    {path: 'order', component: OrderComponent},
+    {path: 'about', loadChildren: './about/about.module#AboutModule'},
+    {path: 'order', loadChildren: './order/order.module#OrderModule'},
     {path: 'order-summary', component: OrderSummaryComponent}
 
 ]
